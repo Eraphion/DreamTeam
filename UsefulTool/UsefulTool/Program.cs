9bd2df45c1b9be4d4a8c71500e37885799000ce4
Trusov Evgeny Dmitrievich <official.trusov.mail@gmail.com>
2017-03-05 21:44:44 +0700

    MainUpgrated

---
+++ UsefulTool/UsefulTool/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsefulTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("InitCommit");
+            Console.WriteLine("SecondCommit");
+        }
+    }
+}
